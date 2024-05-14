3f9866de454c7fa3f1634d52ea4730fc10750827
diogo <diogooonair@gmail.com>
2024-05-14 21:42:42 +0100

    Sprint 1

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'Paginas')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Paginas', 'login.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Paginas', 'login.html'));
+});
+app.get('/registar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Paginas', 'registo.html'));
+});
+
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
